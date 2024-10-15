92c208ed8a0beaf1354773e966cd7b4381906200
paulorobertorodrigues <pauloroberto23061985@gmail.com>
2024-10-15 12:02:48 -0300

    INIT: Criação das paginas de login mobile

---
+++ src/components/button/button.style.js
@@ -0,0 +1,15 @@
+ import {COLORS, FONT_SIZE} from "../../constants/theme.js";
+ 
+ export const styles = {
+    btn: {
+        whidth: "100%",
+        backgroundColor: COLORS.blue,
+        borderRadius: 6,
+        padding: 12
+    },
+    text: {
+        color:"#fff",
+        fontSize: FONT_SIZE.md,
+        textAlign:"center"
+    }
+}
---
+++ src/screens/login/login.style.js
@@ -0,0 +1,33 @@
+import {COLORS, FONT_SIZE} from "../../constants/theme.js";
+ 
+export const styles = {
+    container: {
+        backgroundColor: COLORS.white,
+        flex: 1,
+        padding:50,
+        justifyContent: "space-between"
+    },
+    containerLogo: {
+        alignItems: "center"
+    },
+  logo: {
+    width: 150,
+    height: 30
+  },
+  containerInput: {
+    marginBottom: 15
+  },
+  input: {
+    backgroundColor: COLORS.gray5,
+    padding: 10,
+    borderRadius: 6
+  },
+  footer: {
+    alignItems:"center",
+    justifyContent: "center",
+    flexDirection: "row"
+  },
+  footerLink: {
+    color: COLORS.blue
+  }
+}
